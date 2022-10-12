ba339bd05b5784179eb645ec8c84aee19f84ecbb
smhrd <smhrd@119.200.31.122>
2022-10-12 12:05:06 +0900

    상익_1012/1204 commit

--- Project/src/main/java/com/smhrd/mapper/FoodMapper.java
+++ Project/src/main/java/com/smhrd/mapper/FoodMapper.java
@@ -1,10 +1,14 @@
 package com.smhrd.mapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.smhrd.domain.Food;
+import com.smhrd.domain.FoodVO;
+import com.smhrd.domain.Ingredient;
+import com.smhrd.domain.Relationship;
 
 @Mapper
 public interface FoodMapper{
@@ -17,5 +21,14 @@ public interface FoodMapper{
 	// 음식 목록 출력하기
 	public List<Food> foodList(Food food);
 	
+	// ingredient_name autocomplete
+	public List<Ingredient> IngredientAuto(String ingredient);
+	
+	public List<Relationship> RelationList(FoodVO foodVO);
+	
+	public List<Food> FoodList(ArrayList<Integer> intList);
+
+	// food_name autocomplete
+	public List<Food> FoodAuto(String food);
 	
 }
