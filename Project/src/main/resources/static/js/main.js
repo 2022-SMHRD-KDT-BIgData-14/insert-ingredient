26de8cc0ad1b5bc9a0ce055f538168f55e7d1f28
smhrd <smhrd@222.102.104.49>
2022-10-12 10:18:21 +0900

    세령-update 페이지 삽입 및 수정

--- Project/src/main/resources/static/js/main.js
+++ Project/src/main/resources/static/js/main.js
@@ -14,10 +14,6 @@ function goMypage(){
 	location.href="goMypage.do";
 }
 
-/* 임시 업로드로 이동 함수 */
-function goUpload_a(){
-	location.href="goUpload_a.do";
-}
 
 /* 임시 메인으로 이동 함수 */
 function goMain_a(){
